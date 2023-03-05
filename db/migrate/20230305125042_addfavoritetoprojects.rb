306b3c49adea3d59c09407a56f3f70fc0d645e23
treva556 <marktreva92@gmail.com>
2023-03-05 15:52:15 +0300

    migration tables

---
+++ db/migrate/20230305125042_addfavoritetoprojects.rb
@@ -0,0 +1,5 @@
+class Addfavoritetoprojects < ActiveRecord::Migration[7.0]
+  def change
+    add_column :projects, :favorite, :boolean
+  end
+end
---
+++ db/migrate/20230305125111_addcolortoprojects.rb
@@ -0,0 +1,5 @@
+class Addcolortoprojects < ActiveRecord::Migration[7.0]
+  def change
+    add_column :projects, :color, :string
+  end
+end
